9be0d992ed7f8f3f1c71e4af1d7f76ab996483d6
Skyler Grey <skyler.grey@collabora.com>
2023-07-20 12:18:12 +0000

    Add websocket support and a tunnel server

--- src/main/java/com/collabora/productivity/skyler/sidecar/App.java
+++ src/main/java/com/collabora/productivity/skyler/sidecar/App.java
@@ -11,60 +11,65 @@ import com.sun.star.uno.UnoRuntime;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Throwable {
         System.out.println("Hello world!");
-            // get the remote office component context
-            com.sun.star.uno.XComponentContext xContext;
-            try {
-                xContext = com.sun.star.comp.helper.Bootstrap.bootstrap();
-            } catch (BootstrapException e) {
-                // TODO Auto-generated catch block
-                System.out.println(e.getMessage());
-                throw e.getTargetException();
+        // get the remote office component context
+        com.sun.star.uno.XComponentContext xContext;
+        try {
+            xContext = com.sun.star.comp.helper.Bootstrap.bootstrapWebsocketConnection(false, "localhost", 9980);
+        } catch (BootstrapException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+
+            if (e.getTargetException().getCause() != null) {
+                throw e.getTargetException().getCause();
             }
 
-            System.out.println("Connected to a running office ...");
+            throw e.getTargetException();
+        }
 
-            com.sun.star.lang.XMultiComponentFactory xMCF =
-                xContext.getServiceManager();
+        System.out.println("Connected to a running office ...");
 
-            if (xMCF == null) {
-                System.out.println( "remote ServiceManager is not available" );
-                System.exit(1);
+        com.sun.star.lang.XMultiComponentFactory xMCF =
+            xContext.getServiceManager();
+
+        if (xMCF == null) {
+            System.out.println( "remote ServiceManager is not available" );
+            System.exit(1);
+        }
+
+        System.out.println("Got the remote ServiceManager");
+
+        String loadUrl = "private:factory/swriter";
+        Object desktop = xMCF.createInstanceWithContext("com.sun.star.frame.Desktop", xContext);
+        XComponentLoader xComponentLoader = (XComponentLoader) UnoRuntime.queryInterface(
+            XComponentLoader.class, desktop);
+        PropertyValue[] loadProps = new PropertyValue[0];
+    
+
+        XComponent xWriterComponent = xComponentLoader.loadComponentFromURL(loadUrl, "_blank", 0, loadProps);
+        XTextDocument xTextDocument = (XTextDocument)UnoRuntime.queryInterface(
+            XTextDocument.class, xWriterComponent);
+        XText xText = xTextDocument.getText();
+
+        int x = 0;
+        XTextCursor cursor = xText.createTextCursor();
+        String lipsum = "\n" + //
+                "\n" + //
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac consequat justo, non fermentum felis. Suspendisse rutrum dui suscipit blandit egestas. Sed ultrices tortor dolor, eu sodales ex volutpat non. Sed faucibus nisl vitae tellus volutpat, et efficitur massa vulputate. Phasellus sed vehicula est, eu iaculis sapien. Phasellus id accumsan nisl, vel lacinia odio. Curabitur enim dui, bibendum a bibendum mattis, interdum eu turpis. Etiam nec lectus egestas libero consequat rhoncus eu vel eros. Duis in fringilla justo, a euismod felis. Donec eget sodales nibh. Suspendisse vestibulum orci lectus. Cras pellentesque feugiat massa iaculis vestibulum.\n" + //
+                "\n" + //
+                "Fusce finibus tempus risus, et facilisis quam ullamcorper sit amet. Nullam ac massa eget mi dignissim fermentum in congue erat. Aenean ornare, neque non luctus consectetur, diam dui rhoncus est, fringilla cursus ante elit ut ligula. Sed pellentesque ornare arcu, nec luctus sapien imperdiet a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus efficitur mattis sapien, sit amet lacinia diam pulvinar eget. Sed lacinia in eros lobortis vestibulum. Nam ullamcorper vulputate lectus in porttitor. In turpis orci, sagittis eget lacus in, viverra suscipit arcu. Praesent diam lorem, faucibus quis nisl a, maximus luctus nisl.\n" + //
+                "\n" + //
+                "Nam consequat interdum tincidunt. Vestibulum id velit a sapien posuere sodales dapibus eu orci. Aliquam vitae nulla facilisis, ultrices neque ut, elementum urna. Mauris consequat leo at neque porta porta. Nam diam erat, tincidunt non blandit iaculis, auctor ut quam. Vestibulum vel mi vel lorem vulputate dictum at id urna. In eu venenatis neque. Donec eget placerat lacus, at luctus lacus. Integer blandit volutpat ligula, vitae lobortis est malesuada ut.\n" + //
+                "\n" + //
+                "Nam ac laoreet dui. Etiam ut turpis maximus massa condimentum eleifend ut ut nisi. Nam et sodales leo. Cras sollicitudin mauris et suscipit tempus. In commodo faucibus mauris at aliquet. Pellentesque at consequat lorem. In interdum diam sit amet ante. ";
+        while (true) {
+            for (char character : lipsum.toCharArray()) {
+                Thread.sleep(10);
+                xText.insertString(cursor, Character.toString(character), false);
+                cursor.gotoEnd(false);
             }
-
-            System.out.println("Got the remote ServiceManager");
-
-            String loadUrl = "private:factory/swriter";
-            Object desktop = xMCF.createInstanceWithContext("com.sun.star.frame.Desktop", xContext);
-            XComponentLoader xComponentLoader = (XComponentLoader) UnoRuntime.queryInterface(
-                XComponentLoader.class, desktop);
-            PropertyValue[] loadProps = new PropertyValue[0];
-        
-
-            XComponent xWriterComponent = xComponentLoader.loadComponentFromURL(loadUrl, "_blank", 0, loadProps);
-            XTextDocument xTextDocument = (XTextDocument)UnoRuntime.queryInterface(
-                XTextDocument.class, xWriterComponent);
-            XText xText = xTextDocument.getText();
-
-            int x = 0;
-            XTextCursor cursor = xText.createTextCursor();
-            String lipsum = "\n" + //
-                    "\n" + //
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac consequat justo, non fermentum felis. Suspendisse rutrum dui suscipit blandit egestas. Sed ultrices tortor dolor, eu sodales ex volutpat non. Sed faucibus nisl vitae tellus volutpat, et efficitur massa vulputate. Phasellus sed vehicula est, eu iaculis sapien. Phasellus id accumsan nisl, vel lacinia odio. Curabitur enim dui, bibendum a bibendum mattis, interdum eu turpis. Etiam nec lectus egestas libero consequat rhoncus eu vel eros. Duis in fringilla justo, a euismod felis. Donec eget sodales nibh. Suspendisse vestibulum orci lectus. Cras pellentesque feugiat massa iaculis vestibulum.\n" + //
-                    "\n" + //
-                    "Fusce finibus tempus risus, et facilisis quam ullamcorper sit amet. Nullam ac massa eget mi dignissim fermentum in congue erat. Aenean ornare, neque non luctus consectetur, diam dui rhoncus est, fringilla cursus ante elit ut ligula. Sed pellentesque ornare arcu, nec luctus sapien imperdiet a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus efficitur mattis sapien, sit amet lacinia diam pulvinar eget. Sed lacinia in eros lobortis vestibulum. Nam ullamcorper vulputate lectus in porttitor. In turpis orci, sagittis eget lacus in, viverra suscipit arcu. Praesent diam lorem, faucibus quis nisl a, maximus luctus nisl.\n" + //
-                    "\n" + //
-                    "Nam consequat interdum tincidunt. Vestibulum id velit a sapien posuere sodales dapibus eu orci. Aliquam vitae nulla facilisis, ultrices neque ut, elementum urna. Mauris consequat leo at neque porta porta. Nam diam erat, tincidunt non blandit iaculis, auctor ut quam. Vestibulum vel mi vel lorem vulputate dictum at id urna. In eu venenatis neque. Donec eget placerat lacus, at luctus lacus. Integer blandit volutpat ligula, vitae lobortis est malesuada ut.\n" + //
-                    "\n" + //
-                    "Nam ac laoreet dui. Etiam ut turpis maximus massa condimentum eleifend ut ut nisi. Nam et sodales leo. Cras sollicitudin mauris et suscipit tempus. In commodo faucibus mauris at aliquet. Pellentesque at consequat lorem. In interdum diam sit amet ante. ";
-            while (true) {
-                for (char character : lipsum.toCharArray()) {
-                    Thread.sleep(10);
-                    xText.insertString(cursor, Character.toString(character), false);
-                    cursor.gotoEnd(false);
-                }
-            }
+        }
     }
 }
 
